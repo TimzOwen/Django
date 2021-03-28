54ec93a20873492135b9219a579b1a6d44494cc1
TimzOwen <ktimothy@kabarak.ac.ke>
2021-03-28 13:24:53 +0300

    updated the urls

--- timzowen/myapp/views.py
+++ timzowen/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from djago.https import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world, you are index my app")
